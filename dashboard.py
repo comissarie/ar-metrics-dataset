5f62a560a75f5b80756790795eff7861cdfe7ac7
Nikita K <84910245+comissarie@users.noreply.github.com>
2024-10-26 21:11:14 +0300

    Add gen2 dashboard

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,6 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import numpy as np
 from datetime import datetime, timedelta
 
 def load_data():
